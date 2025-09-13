4bc0de56938f12adfc97731cc64b9116f11f6aa9
Manimaran Ramakrishnan <r.manimaran@yahoo.co.in>
2025-09-13 18:22:18 -0400

    Added HealthChecks for the Sql server.

---
+++ AI.SQLServerEmbeddingsApp/WebApi/CheckSqlConnection.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace WebApi;
+
+public class CheckSqlConnection : IHealthCheck
+{
+    private readonly string _connectionString;
+
+    public CheckSqlConnection(IConfiguration configuration)
+    {
+        _connectionString = configuration.GetConnectionString("DefaultConnection") ?? string.Empty;
+    }
+    public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+    {
+        try 
+        {             
+            using var connection = new SqlConnection(_connectionString);
+
+            await connection.OpenAsync();
+            using var command = new SqlCommand("SELECT 1", connection);
+            await command.ExecuteScalarAsync();
+
+            return HealthCheckResult.Healthy("SQL Database is reachable.");
+        }
+        catch (Exception ex)
+        {
+            return new HealthCheckResult(status:context.Registration.FailureStatus, description:"SQL Database is not reachable.", exception:ex);
+        }
+    }
+}
--- AI.SQLServerEmbeddingsApp/WebApi/Program.cs
+++ AI.SQLServerEmbeddingsApp/WebApi/Program.cs
@@ -1,12 +1,16 @@
 using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Options;
 using OllamaSharp;
+using WebApi;
 using WebApi.Appsettings;
 using WebApi.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+
+// builder.Services.AddAzureOpenAIEmbeddingGenerator(deploymentName: "text-embedding-3-small", apiKey: builder.Configuration["AzureOpenAI:ApiKey"], endpoint: builder.Configuration["AzureOpenAI:Endpoint"]);
 
 builder.Services.Configure<AISettings>(builder.Configuration.GetSection(AISettings.SectionName));
 
@@ -18,7 +22,9 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 
 builder.Services.AddSqlServerVectorStore(o=>connectionString);
 
-    
+builder.Services.AddHealthChecks()
+    .AddCheck<CheckSqlConnection>("SQL Database");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -26,6 +32,28 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.MapHealthChecks("/health", new Microsoft.AspNetCore.Diagnostics.HealthChecks.HealthCheckOptions
+{
+   ResponseWriter = async (context,report) => {
+       
+           context.Response.ContentType = "application/json";
+
+           var response = new
+           {
+               status = report.Status.ToString(),
+               results = report.Entries.Select(e => new
+               {
+                   component = e.Key,
+                   status = e.Value.Status.ToString(),
+                   description = e.Value.Description,
+                   duration = e.Value.Duration.ToString()
+               }),
+               TototalDuration = report.TotalDuration
+           };
+           await context.Response.WriteAsJsonAsync(response);
+       }
+});
 
 app.UseHttpsRedirection();
 
