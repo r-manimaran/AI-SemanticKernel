9e2758a2b08923cf5611c79728bc90e6b8679ca2
Manimaran Ramakrishnan <r.manimaran@yahoo.co.in>
2026-03-13 17:30:47 -0400

    Use AgentFramework to call Database MCP tools

---
+++ StreamableHttpMCP/AgentFrameworkMcpToolCallDatabase/Program.cs
@@ -0,0 +1,65 @@
+﻿using AgentFrameworkMcpToolCallDatabase;
+using Azure.AI.OpenAI;
+using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+using ModelContextProtocol.Client;
+using OpenAI.Chat;
+using System.Text;
+
+AzureOpenAIClient client = new AzureOpenAIClient(new Uri(LLMConfig.Endpoint),
+                            new System.ClientModel.ApiKeyCredential(LLMConfig.ApiKey));
+
+// Create the MCP Client
+McpClient databaseMcpClient = await McpClient.CreateAsync(new HttpClientTransport(new HttpClientTransportOptions
+{
+    Endpoint = new Uri("http://localhost:5200/mcp/"),
+    TransportMode = HttpTransportMode.StreamableHttp,
+}));
+
+IList<McpClientTool> toolsInDBMcp = await databaseMcpClient.ListToolsAsync();
+
+AIAgent agent = client.GetChatClient(LLMConfig.DeploymentOrModelId)
+    .AsAIAgent(
+    instructions: "You are a Database expert in creating DB Queries. Analyze the user query and use appropriate tools from the MCP",
+    tools: toolsInDBMcp.Cast<AITool>().ToList()
+    ).AsBuilder()
+    .Use(FunctionCallingMiddleware)
+    .Build();
+
+while (true)
+{
+    Console.Write("User:>");
+    string userInput = Console.ReadLine() ?? string.Empty;
+    if (string.IsNullOrEmpty(userInput))
+    {
+        break;
+    }
+    var userMessage = new Microsoft.Extensions.AI.ChatMessage(ChatRole.User, userInput);
+    AgentResponse response = await agent.RunAsync(userMessage);
+    Console.WriteLine($"Agent:>{response}");
+    Utils.Separator();
+}
+
+async ValueTask<object?> FunctionCallingMiddleware(AIAgent callingAgent, FunctionInvocationContext context,
+    Func<FunctionInvocationContext, CancellationToken, ValueTask<object?>> next,
+    CancellationToken token)
+{
+    StringBuilder functionCallDetails = new();
+    functionCallDetails.Append($"-Tool Call: '{context.Function.Name}'");
+    if (context.Arguments != null && context.Arguments.Count > 0)
+    {
+        functionCallDetails.AppendLine();
+        functionCallDetails.AppendLine("-With Arguments:");
+        functionCallDetails.AppendLine(string.Join(Environment.NewLine, context.Arguments.Select(kv => $"  - {kv.Key}: {kv.Value}")));
+    }
+    Utils.WriteLineInformation(functionCallDetails.ToString());
+    try
+    {
+        return await next(context, token);
+    }
+    catch (Exception ex)
+    {
+        Utils.WriteLineError($"Tool call failed: {ex.Message}");
+        throw;
+    }
+}
--- StreamableHttpMCP/README.md
+++ StreamableHttpMCP/README.md
@@ -1,4 +1,5 @@
 # MCP Server with Streamable Http 
+![alt text](image-13.png)
 
 ![alt text](image.png)
 
@@ -67,3 +68,9 @@ Here are some examples
 ![alt text](image-11.png)
 
 ![alt text](image-12.png)
+
+![alt text](image-14.png)
+
+![alt text](image-15.png)
+
+![alt text](image-16.png)
